6f38411a04c366e75267a57690739faeab549613
ravics02 <73768630+ravics02@users.noreply.github.com>
2020-11-04 08:58:49 +1300

    use loop to create html element

--- htmlcodeonly.php
+++ htmlcodeonly.php
@@ -7,5 +7,18 @@
 <body>
 <H1> This is another way to write PHP</H1>
 <p> I will put my paragraph below:</p>
+<?PHP
+$i = 0;
+While ($1<10){
+
+    echo"<p>This is the".$i."th line</p>>";
+
+$i++;
+
+
+}
+
+?>
+?/>
 </body>
 </html>
