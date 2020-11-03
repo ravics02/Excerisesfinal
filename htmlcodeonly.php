ec8ad48600ea6142eb227f4436f92d6a66b38c48
ravics02 <73768630+ravics02@users.noreply.github.com>
2020-11-04 08:45:04 +1300

    I created a html page in php

---
+++ htmlcodeonly.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>HTML CODE ONLY </title>
+</head>
+<body>
+<H1> This is another way to write PHP</H1>
+<p> I will put my paragraph below:</p>
+</body>
+</html>
