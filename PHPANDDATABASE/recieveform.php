42f32259731a883daad67d295518a7c406fe00a4
ravics02 <73768630+ravics02@users.noreply.github.com>
2020-11-05 10:07:06 +1300

    I created a login for my company portal form

---
+++ PHPANDDATABASE/recieveform.php
@@ -0,0 +1,10 @@
+<?php
+
+//This is the php for the My company portal [The file name for lei :login.php]
+$username =$_GET["username"];// in side the square brackets, should be the name of the input box
+$Password =$_GET["Password"];//  the name od the input box
+
+
+
+echo "Username:".$username;
+echo "<br> Password:".$Password;
