f4f66a6fefceceadf26540c0cb503eebc694b7a2
ravics02 <73768630+ravics02@users.noreply.github.com>
2020-11-09 09:56:39 +1300

    created session destory

--- PHPANDDATABASE/SessionExcerises/index.php
+++ PHPANDDATABASE/SessionExcerises/index.php
@@ -29,11 +29,11 @@ if (isset($_POST["Message"])) {
 <body>
 
 <ul>
-    <li><a> href="page1.php"> Page 1 </a></li>
-    <li><a> href="page2.php"> Page 2 </a></li>
-    <li><a> href="page3.php"> Page 3 </a></li>
-    <li><a> href="page4.php"> Page 4 </a></li>
-    <li><a> href="page5.php"> Page 5 </a></li>
+    <li><a href="page1.php"> Page 1 </a></li>
+    <li><a href="page2.php"> Page 2 </a></li>
+    <li><a href="page3.php"> Page 3 </a></li>
+    <li><a href="page4.php"> Page 4 </a></li>
+    <li><a href="page5.php"> Page 5 </a></li>
 
 
 </ul>
