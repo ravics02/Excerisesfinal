2c64dfc5eb230764a97ddac624ce7b115cb42bbf
ravics02 <73768630+ravics02@users.noreply.github.com>
2020-11-10 09:18:12 +1300

    index page

--- Include/menu.php
+++ Include/menu.php
@@ -2,8 +2,8 @@
 <ul>
 
 <li><a href="index.php"> Home</a> </li>
-    <li><a href="about.php"> Home</a> </li>
-    <li><a href="products.php"> Home</a> </li>
+    <li><a href="about.php"> about</a> </li>
+    <li><a href="products.php"> products</a> </li>
 
 
 </ul>
