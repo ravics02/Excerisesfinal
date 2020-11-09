46039c1d9babf701d52d9c9a4a6b52fa375c906e
ravics02 <73768630+ravics02@users.noreply.github.com>
2020-11-10 10:40:15 +1300

    index page

--- Include/page 3.php
+++ Include/page 3.php
@@ -1,48 +1,48 @@
 <?php
 
-if isset($_POST["username"])&& isset($_POST["password"])){
+if (isset($_POST["username"]) && isset($_POST["password"])) {
     $username = $_POST["username"];
-    $password = $_POST["password"]   ;
+    $password = $_POST["password"];
 
     // create a database connection
 
-    $server ="lolyz0ok3stvj6f0.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+    $server = "lolyz0ok3stvj6f0.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
     $dbusername = "zwtccshi5x8h5pvx";
     $dbpassword = "j6idgpckpu4s42xh";
     $dbname = "j70b2olzmcxne6ap";
-    $conn = new mysqli($server,$dbusername,$dbpassword,$dbname);
+    $conn = new mysqli($server, $dbusername, $dbpassword, $dbname);
 
 
     // create a query
 
-    $sql = "select * from users";
+    $sql = "select * from users;
     where username = '$username' and
-    $password = '$password';
+    $password = '$password'";
 
     //run the query
 
-    $result = mysqli_query($conn,$sql);
+    $result = mysqli_query($conn, $sql);
 
     //show results
 
     if ($result->num_rows == 1) {
-    echo "you have login";
-    while ($row = $result->fetch_assoc()) {
-        echo $row["firstname"];
-        @session_start();
-        $_SESSION["firstname"] = $row["firstname"];
-    }
-} else {
-    echo "wrong username or password";
+        echo "you have login";
+        while ($row = $result->fetch_assoc()) {
+            echo $row["firstname"];
+            @session_start();
+            $_SESSION["firstname"] = $row["firstname"];
+        }
+    } else {
+        echo "wrong username or password";
 
-}
+    }
+    }
 ?>
 
-<form action="<?php $_SERVER["PHP_SELF"]; ?> "METHOD="post">
-    <input name="username"type="text" placeholder="username">
-    <input name="password"type="text" placeholder="password">
+<form action="<?php $_SERVER["PHP_SELF"]; ?> " METHOD="post">
+    <input name="username" type="text" placeholder="username">
+    <input name="password" type="text" placeholder="password">
     <input type="submit" value="post">
 
 </form>
 
-?>
