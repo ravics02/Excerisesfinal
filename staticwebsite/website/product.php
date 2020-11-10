cd79d6d70066be6768c6298b95ab8f6017e7781c
ravics02 <73768630+ravics02@users.noreply.github.com>
2020-11-11 10:39:44 +1300

    created product page

---
+++ staticwebsite/website/product.php
@@ -0,0 +1,22 @@
+<?php
+$server ="lolyz0ok3stvj6f0.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+$dbusername = "zwtccshi5x8h5pvx";
+$dbpassword = "j6idgpckpu4s42xh";
+$dbname = "j70b2olzmcxne6ap";
+
+$conn = new mysqli($server,$dbusername,$dbpassword,$dbname);
+
+
+$sql = "select * from product";
+
+$result = mysqli_query($conn,$sql);
+
+while ($row = $result->fetch_assoc()){
+
+    echo $row["id"];
+    echo $row["productName"];
+
+
+
+
+}
