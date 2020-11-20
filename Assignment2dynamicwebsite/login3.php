6f18ff773c7cd4bac1005fade03ed43921ba822a
ravics02 <73768630+ravics02@users.noreply.github.com>
2020-11-20 16:15:05 +1300

    Dynamicwebsite

--- Assignment2dynamicwebsite/login3.php
+++ Assignment2dynamicwebsite/login3.php
@@ -1,56 +1,68 @@
 <?php
+
+
 if (isset($_POST["username"]) && isset($_POST["password"])) {
     $username = $_POST["username"];
     $password = $_POST["password"];
 
+    // create a database connection
 
-//created a database connection
     $server = "lolyz0ok3stvj6f0.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
     $dbusername = "zwtccshi5x8h5pvx";
     $dbpassword = "j6idgpckpu4s42xh";
     $dbname = "j70b2olzmcxne6ap";
     $conn = new mysqli($server, $dbusername, $dbpassword, $dbname);
 
-    $conn = new mysqli($server, $dbusername, $dbpassword, $dbname);
 
-    if ($conn->error) {
-        echo $conn->error;
-    } else {
-        echo "Connected";
-    }
+    // create a query
 
-//create a query
-    $sql = "select * from users 
-            where username = '$username' and 
-            password = '$password'";
+    $sql = "select * from users;
+    where username = '$username' and
+    $password = '$password'";
 
+    //run the query
 
-    echo
-//run my query
     $result = mysqli_query($conn, $sql);
 
-//show result
+    //show results
+
     if ($result->num_rows == 1) {
         echo "you have login";
         while ($row = $result->fetch_assoc()) {
             echo $row["firstname"];
+            //start session
+            @session_start();
+            $_SESSION["userID"]= $row["id"];
+            $_SESSION["firstname"] = $row["firstname"];
         }
     } else {
         echo "wrong username or password";
+
     }
-}
+    }
+?>
+<?php
+if (!isset($_SESSION["UserID"])){
 
-if (!isset($_SESSION["userID"])) {
+?>
+
+<form action="<?php $_SERVER["PHP_SELF"]; ?> " METHOD="post">
+    <input name="username" type="text" placeholder="username">
+    <input name="password" type="text" placeholder="password">
+    <input type="submit" value="post">
+
+</form>
+
+<?php
+
+} else{
+
     ?>
-    <form action="<?php $_SERVER["PHP_SELF"]; ?>" method="post">
-        <input name="username" type="text" placeholder="Username">
-        <input name="password" type="password" placeholder="Password">
-        <input type="submit" value="Post">
-    </form>
-    <?php
-}else{
+    <a href="logout.php">Logout"</a>
 
-    echo '<a href="logout.php">logout</a>';
+    <?php
 
 }
+
+
 ?>
