f9b157ff1df297b5c251767dfe0caea572727b0c
ravics02 <73768630+ravics02@users.noreply.github.com>
2020-11-21 11:19:27 +1300

    Dynamicwebsite

--- Assignment2dynamicwebsite/aboutus.php
+++ Assignment2dynamicwebsite/aboutus.php
@@ -33,7 +33,8 @@
         <label>
           <input name="password" type="password" placeholder="Password">
         </label>
-        <input type="button" value="Login">
+        <input type="submit" value="Login">
+
       </form>
 
 
